526f86575526e9b84ba0365e7d319b59a61ac2ca
kim.biloni <kim.biloni@he-arc.ch>
2019-03-05 14:21:21 +0100

    usinf viola jones to recognize multiple face

--- BestFace.py
+++ BestFace.py
@@ -1,6 +1,35 @@
 #coding: utf-8
 
+import numpy as np
 import cv2 as cv
+import os
+from dotenv import load_dotenv
+
+def use_camera():
+    cap = cv.VideoCapture(0)
+
+    while(True):
+        ret, frame = cap.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        if ret:
+            vj = viola_jones(gray)
+            cv.imshow('vj', vj)
+        if(cv.waitKey(1) & 0xFF == ord('q')):
+            break
+    cap.release()
+    cv.destroyAllWindows()
+
+def viola_jones(img):
+    face_cascade = cv.CascadeClassifier(os.getenv("FACE_CASCADE_CLASSIFIER", 0))
+    
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
+    for(x, y, w, h) in faces:
+        cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    return img
+
 
 if __name__=="__main__":
     print(cv.__version__)
+    load_dotenv()
+    use_camera()
+    viola_jones()
