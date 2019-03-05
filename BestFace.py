bf0ad5cf6c687245e5515b84572071a13cee3375
kim.biloni <kim.biloni@he-arc.ch>
2019-03-05 14:43:18 +0100

    improve error catching

--- BestFace.py
+++ BestFace.py
@@ -3,6 +3,7 @@
 import numpy as np
 import cv2 as cv
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 
 def use_camera():
@@ -11,21 +12,25 @@ def use_camera():
     while(True):
         ret, frame = cap.read()
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        if ret:
-            vj = viola_jones(gray)
-            cv.imshow('vj', vj)
+        ret, vj = viola_jones(gray)
+        cv.imshow('vj', vj)
         if(cv.waitKey(1) & 0xFF == ord('q')):
             break
     cap.release()
     cv.destroyAllWindows()
 
 def viola_jones(img):
+    detected = False
     face_cascade = cv.CascadeClassifier(os.getenv("FACE_CASCADE_CLASSIFIER", 0))
-    
-    faces = face_cascade.detectMultiScale(img, 1.3, 5)
-    for(x, y, w, h) in faces:
-        cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    return img
+    try:
+        faces = face_cascade.detectMultiScale(img, 1.3, 5)
+        # print(faces) # ex: [[221  91 356 356]]
+        for(x, y, w, h) in faces:
+            detected = True
+            cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    except:
+        pass
+    return detected, img
 
 
 if __name__=="__main__":
