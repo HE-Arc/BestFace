379ecdecc02df392231968e432086b9651123171
kim.biloni <kim.biloni@he-arc.ch>
2019-03-12 15:05:12 +0100

    WIP finalize face detection and photo taking

--- BestFace.py
+++ BestFace.py
@@ -7,6 +7,9 @@ from time import time
 from datetime import datetime
 from dotenv import load_dotenv
 
+from threading import Thread
+from queue import Queue, Empty
+
 def generate_image_directory():
     '''
     Create a unique directory for a sequence of frame
@@ -50,12 +53,69 @@ def viola_jones(img):
         pass
     return detected, img
 
+queue = Queue(20)
+
+class CameraProducerThread(Thread):
+    '''
+    This class provide all the images of a face when detected
+    '''
+    def run(self):
+        global queue
+
+        cap = None
+        try:
+            cap = cv.VideoCapture(0)
+
+            while True:
+                ret, frame = cap.read()
+                gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                ret, vj = viola_jones(gray)
+                if ret:
+                    queue.put(frame)
+        finally:
+            cap.release()
+
+
+class PhotoTakerConsumerThread(Thread):
+    '''
+    This class save a defined number of picture
+    '''
+    
+    def run(self):
+        global queue
+
+        sequence_running = False
+        dir_path = None
+        img_num = 0
+        while True:
+            try:
+                img = queue.get_nowait()
+
+                if not sequence_running:
+                    sequence_running = True
+                    dir_path = generate_image_directory()
+
+                img_path = os.path.join(dir_path, "img" + str(img_num) + ".png")
+                cv.imwrite(img_path, img, [int(cv.IMWRITE_PNG_COMPRESSION), 9])
+                
+                img_num += 1
+                queue.task_done()
+            except Empty:
+                sequence_running = False
+                img_num = 0
+            pass
+
 
 if __name__=="__main__":
     print(cv.__version__)
-    '''
     load_dotenv()
+    '''
     use_camera()
     viola_jones()
     '''
-    print(generate_image_directory())
+    
+    producer = CameraProducerThread()
+    consumer = PhotoTakerConsumerThread()
+
+    producer.start()
+    consumer.start()
