719395e940b4fe9e6dcab9ae14c1df75602d7fd8
kim.biloni <kim.biloni@he-arc.ch>
2019-03-12 14:03:08 +0100

    unique directory creator

--- BestFace.py
+++ BestFace.py
@@ -3,8 +3,23 @@
 import numpy as np
 import cv2 as cv
 import os
+from time import time
 from datetime import datetime
 from dotenv import load_dotenv
+
+def generate_image_directory():
+    '''
+    Create a unique directory for a sequence of frame
+    return the directory
+    '''
+    # Generate unique directory name
+    directory_name = "bestface_" + datetime.now().timestamp().__int__().__str__()
+
+    # Create directory using the current working dir
+    path = os.path.join(os.getcwd(), directory_name)
+    os.mkdir(path)
+
+    return path
 
 def use_camera():
     cap = cv.VideoCapture(0)
@@ -13,6 +28,9 @@ def use_camera():
         ret, frame = cap.read()
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         ret, vj = viola_jones(gray)
+        if ret:
+            name = "img_" + time().__str__() + ".png"
+            cv.imwrite(name, frame, [int(cv.IMWRITE_PNG_COMPRESSION), 9])
         cv.imshow('vj', vj)
         if(cv.waitKey(1) & 0xFF == ord('q')):
             break
@@ -35,6 +53,9 @@ def viola_jones(img):
 
 if __name__=="__main__":
     print(cv.__version__)
+    '''
     load_dotenv()
     use_camera()
     viola_jones()
+    '''
+    print(generate_image_directory())
